83cad3ad1684295530f104066fc88ac2903bc5e5
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-26 14:45:10 +0200

    create express server and add new input in html page relates #3 #4

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { join } = require('path');
+
+const app = express();
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 4000);
+app.use(express.static(join(__dirname, '..', 'public')));
+
+module.exports = app;
