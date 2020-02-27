d55b27bfaa365b4f7de00eacbe5cfcd63d8f4006
rehabas <reremo45@gmail.com>
2020-02-27 15:19:59 +0200

    post books relates #39

--- src/controllers/addBook.js
+++ src/controllers/addBook.js
@@ -1,8 +1,8 @@
 const { postData } = require('../database/queries/postData');
 
 exports.addBook = (req, res) => {
-  postData(req.json()).then((data) => {
-    console.log(data);
-    res.redirect('/');
-  });
-}
+  const bookInfo = req.body;
+  postData(bookInfo)
+    .then(() => res.redirect('/'))
+    .catch((err) => console.log('err:', err));
+};
--- src/database/queries/postData.js
+++ src/database/queries/postData.js
@@ -3,8 +3,8 @@ const dbConnection = require('../config/db_connection');
 const postData = (reqData) => {
   const { title, cover, category, author, publisher } = reqData;
   const sql = {
-    text1: 'INSERT INTO books (title, publisher_id, category, reserved, thumbnail) VALUES ($1, $2, $3, $4, $5);',
-    values1: [title, cover, category, author, publisher],
+    text: 'INSERT INTO books (title, category, reserved, thumbnail) VALUES ($1, $2, $3, $4);',
+    values: [title, category, false, cover],
     text2: 'INSERT INTO authors (name) VALUES ($1);',
     values2: [author],
     text3: 'INSERT INTO publishers (name) VALUES ($1);',
