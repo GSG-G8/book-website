7c6e91d6a2c613a66031778a502386ee63211b23
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-27 15:07:17 +0200

    get data from back-end by fetch relates #39

--- src/database/config/db_connection.js
+++ src/database/config/db_connection.js
@@ -7,12 +7,10 @@ let dbUrl;
 if (process.env.NODE_ENV === 'test') {
   dbUrl = process.env.TEST_DB_URL;
 } else if (process.env.NODE_ENV === 'production') {
-  dbUrl = process.env.PRODUCTION_DB_URL;
+  dbUrl = process.env.DATABASE_URL;
 } else if (process.env.NODE_ENV === 'development') {
   dbUrl = process.env.DB_URL;
 }
-console.log(dbUrl);
-
 const options = {
   connectionString: dbUrl,
 };
