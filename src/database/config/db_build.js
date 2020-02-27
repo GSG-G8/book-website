bcade4315d9255152d8a8d9ddf5adb8652f53285
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-27 13:23:13 +0200

    merge

--- src/database/config/db_build.js
+++ src/database/config/db_build.js
@@ -3,10 +3,9 @@ const { join } = require('path');
 
 const connection = require('./db_connection');
 
+const sql = readFileSync(join(__dirname, 'db_build.sql')).toString();
 
-const dbBuild = () => {
-  const sql = readFileSync(join(__dirname, 'db_build.sql')).toString();
-  return connection.query(sql);
-};
-
-module.exports = dbBuild;
+connection
+  .query(sql)
+  .then(() => console.log('build created successfully!'))
+  .catch((e) => console.error('failed to build', e.stack));
